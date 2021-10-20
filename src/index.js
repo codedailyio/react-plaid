5363d51b421bd85e0f377a2efcdf3412de0b3e0b
Vasiliy <vasili.melnik@melio.com>
2021-10-21 00:33:59 +0200

    added accountSubtypes

--- src/index.js
+++ src/index.js
@@ -22,6 +22,9 @@ class ReactPlaid extends Component {
       legalName: PropTypes.string,
       emailAddress: PropTypes.string
     }),
+    accountSubtypes: PropTypes.shape({
+      depository: PropTypes.arrayOf(PropTypes.string),
+    }),
     selectAccount: PropTypes.bool,
     webhook: PropTypes.string,
     onSuccess: PropTypes.func.isRequired,
@@ -54,6 +57,7 @@ class ReactPlaid extends Component {
       token: this.props.token,
       user: this.props.user,
       selectAccount: this.props.selectAccount,
+      accountSubtypes: this.props.accountSubtypes,
       webhook: this.props.webhook,
       onLoad: this.handleLoad,
       onSuccess: this.handleSuccess,
